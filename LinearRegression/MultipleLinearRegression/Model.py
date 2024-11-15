4730f67f4ff7a7f40c89349aa5470bfc2c24dd7d
Ram <nagaram.annavarapu@gmail.com>
2024-11-15 17:48:31 -0500

    Model done and moving to metrics evaluation

--- LinearRegression/MultipleLinearRegression/Model.py
+++ LinearRegression/MultipleLinearRegression/Model.py
@@ -3,6 +3,7 @@ import numpy as np
 from MLR import ReqData
 import sklearn
 from sklearn import linear_model
+from sklearn.model_selection import train_test_split
 
 
 class Model:
@@ -19,6 +20,11 @@ class Model:
         model=linear_model.LinearRegression()
         x = self.df[['GRE Score', 'TOEFL Score', 'CGPA']]
         y=self.df['Chance of Admit']
+        x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=22) #setting random state to select same data everytime for training/testing.
+        model.fit(x_train,y_train)
+        y_pred=model.predict(x_test)
+        pdt=model.predict([[324,107,8.87]]) #sample prediction
+        print(pdt)
     
     def run_all(self):
         self.connection_check()
